edaf38d1fdf74109f4103c9cacff2f6cf3ed3419
GautaA <antoniogauta@gmail.com>
2019-12-05 00:48:28 +0100

    Cleared the code

--- Day4/src/Day4_1.java
+++ Day4/src/Day4_1.java
@@ -1,49 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day4_1 {
 
     public static void main(String[] args) {
-        int upperLimit = 790572;
-        int lowerLimit = 245182;
-
-        int possibleNumbers = 0;
+        int upperLimit = 790572, lowerLimit = 245182, possibleNumbers = 0;
 
         for (int i = lowerLimit; i <= upperLimit; i++) {
-            if (checkAdjacentDigits(i)) {
-                possibleNumbers++;
-            }
+            if (checkIfOrdered(Integer.toString(i)) && checkDigits(Integer.toString(i))) possibleNumbers++;
         }
-
         System.out.println(possibleNumbers);
     }
 
-    public static boolean checkAdjacentDigits(int number) {
+    private static boolean checkIfOrdered(String number) {
+        String sortedNumber = Stream.of(number.split("")).sorted().collect(Collectors.joining());
+        return number.equals(sortedNumber) ? true : false;
+    }
 
-        List<Integer> passedDigits = new ArrayList<Integer>();
-        boolean lucky = false;
-        boolean ascending = false;
-
-        while (number > 0) {
-            int digit = number % 10;
-
-            if (passedDigits.size() >= 1) {
-                if (passedDigits.get(passedDigits.size() - 1) >= digit) {
-                    ascending = true;
-                }
-                else return false;
-            }
-
-            if (!passedDigits.isEmpty()) {
-                if (passedDigits.get(passedDigits.size() - 1) == digit) {
-                    lucky = true;
-                }
-                passedDigits.add(digit);
-            } else {
-                passedDigits.add(digit);
-            }
-            number /= 10;
+    private static boolean checkDigits(String number) {
+        String[] aList = number.split("");
+        for (int i = 0; i < aList.length - 1; i++) {
+            if (aList[i].equals(aList[i+1])) return true;
         }
-       return lucky & ascending;
+        return false;
     }
 }
--- Day4/src/Day4_2.java
+++ Day4/src/Day4_2.java
@@ -1,85 +1,40 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day4_2 {
 
     public static void main(String[] args) {
-        int upperLimit = 790572;
-        int lowerLimit = 245182;
-
-        int possibleNumbers = 0;
+        int upperLimit = 790572, lowerLimit = 245182, possibleNumbers = 0;
 
         for (int i = lowerLimit; i <= upperLimit; i++) {
-            if (checkAdjacentDigits(i)) {
-                possibleNumbers++;
-            }
+            if (checkIfOrdered(Integer.toString(i)) && checkDigits(Integer.toString(i))) possibleNumbers++;
         }
-
         System.out.println(possibleNumbers);
     }
 
-    public static boolean checkAdjacentDigits(int number) {
-
-        List<Integer> passedDigits = new ArrayList<Integer>();
-        List<Integer> luckyList = new ArrayList<Integer>();
-        boolean lucky = false;
-        boolean ascending = false;
-        boolean luckyStreak = false;
-
-        while (number > 0) {
-            int digit = number % 10;
-
-            if (passedDigits.size() >= 1) {
-                if (passedDigits.get(passedDigits.size() - 1) >= digit) {
-                    ascending = true;
-                }
-                else return false;
-            }
-
-            if (!passedDigits.isEmpty()) {
-                if (passedDigits.get(passedDigits.size() - 1) == digit) {
-                    lucky = true;
-                    if (!luckyList.isEmpty()) {
-                        if (checkIfSame(luckyList.get(luckyList.size() -1), digit)) {
-                            int lastDigit = luckyList.get(luckyList.size() - 1);
-                            String s1 = Integer.toString(lastDigit);
-                            String s2 = Integer.toString(digit);
-                            String s3 = s1 + s2;
-                            luckyList.remove(luckyList.size() - 1);
-                            luckyList.add(Integer.parseInt(s3));
-                        }
-                        else {
-                            int lastDigit = passedDigits.get(passedDigits.size() - 1);
-                            String s1 = Integer.toString(lastDigit);
-                            String s2 = Integer.toString(digit);
-                            String s3 = s1 + s2;
-                            luckyList.add(Integer.parseInt(s3));
-                        }
-                    } else {
-                        int lastDigit = passedDigits.get(passedDigits.size() - 1);
-                        String s1 = Integer.toString(lastDigit);
-                        String s2 = Integer.toString(digit);
-                        String s3 = s1 + s2;
-                        luckyList.add(Integer.parseInt(s3));
-                    }
-                }
-                passedDigits.add(digit);
-            } else {
-                passedDigits.add(digit);
-            }
-            number /= 10;
-        }
-
-        for (int i = 0; i < luckyList.size(); i++) {
-            int numberOfDigits = String.valueOf(luckyList.get(i)).length();
-            if (numberOfDigits == 2) luckyStreak = true;
-        }
-
-        return lucky & ascending & luckyStreak;
+    private static boolean checkIfOrdered(String number) {
+        String sortedNumber = Stream.of(number.split("")).sorted().collect(Collectors.joining());
+        return number.equals(sortedNumber) ? true : false;
     }
 
-    private static boolean checkIfSame(int arrayNumber, int digit) {
-        if (arrayNumber % 10 == digit) return true;
-        else return false;
+    private static boolean checkDigits(String number) {
+        int current = 1;
+        ArrayList<Integer> counts = new ArrayList<Integer>();
+        String[] aList = number.split("");
+        for (int i = 0; i < aList.length - 1; i++) {
+            if (aList[i].equals(aList[i+1])) {
+                current++;
+            } else {
+                counts.add(current);
+                current = 1;
+            }
+            if (i == aList.length - 2) counts.add(current);
+        }
+
+        for (int i = 0; i < counts.size(); i++) {
+            if (counts.get(i) == 2) return true;
+        }
+        return false;
     }
 }
